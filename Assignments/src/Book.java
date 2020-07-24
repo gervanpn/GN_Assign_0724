40b5a94d36a2ec1acb2ffc1985971e9d711ad607
gervanpn <gervann@yahoo.ca>
2020-07-24 19:51:17 -0400

    Finished problem 4

--- Assignments/src/Book.java
+++ Assignments/src/Book.java
@@ -1,4 +1,4 @@
-import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
 
 public class Book {
     private String book_name;
@@ -6,23 +6,31 @@ public class Book {
     private String author;
     private String publisher;
 
-    public Book(String book, String id, String name, String house) {
-        book_name = book;
-        isbn = id;
-        author = name;
-        publisher = house;
+    public Book() {
+        book_name = "Java";
+        isbn = "1020";
+        author = "Oreilly";
+        publisher = "Clearinghouse";
     }
-
+    Scanner scan = new Scanner(System.in);
     public String getBook_name() {
+        System.out.println("Book Name: ");
+        book_name = scan.next();
         return book_name;
     }
     public String getIsbn() {
+        System.out.println("ISBN Number: ");
+        isbn = scan.next();
         return isbn;
     }
     public String getAuthor() {
+        System.out.println("Author: ");
+        author = scan.next();
         return author;
     }
     public String getPublisher() {
+        System.out.println("Publisher: ");
+        publisher = scan.next();
         return publisher;
     }
 
@@ -40,6 +48,7 @@ public class Book {
     }
 
     public void getBookInfo() {
-        System.out.println(book_name + " by " + author + "- published by " + publisher + "(" + isbn + ")");
+        System.out.println(book_name + " written by " + author + " - published by " + publisher + " (" + isbn + ")");
     }
+
 }
