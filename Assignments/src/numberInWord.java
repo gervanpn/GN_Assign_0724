25edf225c4777ef3b3bd0935df28a3b92950e7e2
gervanpn <gervann@yahoo.ca>
2020-08-10 20:22:40 -0400

    Aug 10 assignment added

--- Assignments/src/numberInWord.java
+++ Assignments/src/numberInWord.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class numberInWord {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number 1 to 9: ");
+        System.out.println("Enter a number 0 to 9: ");
         int num = sc.nextInt();
         PrintNumIf(num);
         System.out.println("your number : "+ num + " is " + PrintNumIf(num));
+        PrintNumCase(num);
+        System.out.println("for number "+ num + " is " + PrintNumCase(num));
+
     }
 
-        public static String PrintNumIf(int num) {
+        public static String PrintNumIf(int num) { // Option a
             if(num == 1)
                 return "ONE" ;
             else if(num == 2)
@@ -31,6 +34,33 @@ public class numberInWord {
             else if(num == 0)
                 return "ZERO";
             else
-                return "Other";   //returns if no case is matched
+                return "NOT 0-9";   //returns if no case is matched
         }
+
+    public static String PrintNumCase(int num) {  // Option b
+        switch(num) {
+            case 1 :
+                return "One";
+            case 2:
+                return "Two";
+            case 3:
+                return "Three";
+            case 4:
+                return "Four";
+            case 5:
+                return "Five";
+            case 6:
+                return "Six";
+            case 7:
+                return "Seven";
+            case 8:
+                return "Eight";
+            case 9:
+                return "Nine";
+            case 0:
+                return "Zero";
+            default :
+                return "Other than 0-9";
+        }
+    }
 }
---
+++ Assignments/src/whatDate.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class whatDate {
+    static int days[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    static String name[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input MM & DD - zero to exit");
+
+        for(;;){
+            int m=in.nextInt(), d=in.nextInt();
+            if(m==0&&d==0) break;
+            System.out.println(solve(m,d));
+        }
+    }
+
+    static String solve(int month, int date){
+        int cur = 1;
+        System.out.println("That day is/was a:");
+        for(int i=0; i<month-1; i++) cur += days[i];
+        cur += date;
+        return name[cur%7];
+    }
+}
